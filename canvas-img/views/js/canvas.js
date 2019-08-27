3cdc38616050e2f5ff8e26d757583ee0232655f6
Lenny <lndrschlz@gmail.com>
2019-08-27 21:43:59 +0200

    image formating and resizing

--- canvas-img/views/js/canvas.js
+++ canvas-img/views/js/canvas.js
@@ -50,7 +50,13 @@ function clearArea() {
 
 function inferInput() {
     clearPrint();
-    print("# --- Building Inference ---")
+    print("# --- Building Inference ---");
+    print("# --- reseizing image...");
+    var newImg = resizeImage("myCanvas")
+    print(newImg)
+    print("# --- formating image...");
+    var arrayImg = formatImage(newImg);
+    print(arrayImg)
 }   
 
 /*
@@ -71,3 +77,52 @@ const clearPrint = function () {
     let el = document.getElementsByClassName('output')[0];
     el.innerHTML = "";
 }
+
+const resizeImage = function(canvasId,dimension=28) {
+    var canvas = document.getElementById(canvasId);
+    var context = canvas.getContext("2d");
+    var imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    var i;
+    //var step = canvas.width % dimension
+    var newImageData = ctx.createImageData(dimension, dimension);
+    console.log("imgData.data[i] > " + imgData.data.length)
+    for (i = 0; i < imgData.data.length%dimension; i += 4) {
+        // copy RGBA channels of the pixel
+        //if (imgData.data[i+2] > 0) {
+        console.log("imgData.data[i] > " + imgData.data[i])
+        //}
+        newImageData.data[i] = imgData.data[i];
+        newImageData.data[i+1] = imgData.data[i+1];
+        newImageData.data[i+2] = imgData.data[i+2];
+        newImageData.data[i+3] = imgData.data[i+3];
+    }
+    //ctx.putImageData(imgData, 0, 0)
+    return newImageData;
+}
+/*
+
+*/
+const formatImage = function(imgData,dimension=28) {
+    var i;
+    var imgArray = new Array();
+    var imgSubArray = new Array();
+    for (i = 0; i < imgData.data.length; i += 4) {
+        if (i % (dimension*4) == 0) {
+            // push pixel row and create new array object
+            //console.log("imgData.data.length % dimension == " + i)
+            imgArray.push(imgSubArray);
+            imgSubArray = new Array();
+        }
+        
+        // detect pixels
+        if (imgData.data[i] > 0) {
+            imgSubArray.push(1);
+        }else{
+            imgSubArray.push(0);
+        }
+    
+    }
+    return imgArray
+}
+
