f3cd3c1d70726c27e881ab8471618b20bdd833b7
Lenny <lndrschlz@gmail.com>
2019-08-27 11:14:17 +0200

    image picker for app engine nodejs deployment added

---
+++ imgpick-node/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    //.send('<h2>Upload an Image</h2>')
+    .render('index', { 
+    	title: 'Upload an Image' 
+   	});
+    //.sendFile('/home/lenny/Documents/imgpick-node/index.html')
+    //.end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+module.exports = app;
