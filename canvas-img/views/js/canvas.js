1cdc21a4e708a72a3582d1ae50fed1f655ba7aaa
Lenny <lndrschlz@gmail.com>
2019-08-27 19:59:25 +0200

    added eventlistener to detect button

--- canvas-img/views/js/canvas.js
+++ canvas-img/views/js/canvas.js
@@ -2,7 +2,7 @@ var mousePressed = false;
 var lastX, lastY;
 var ctx;
 
-function InitThis() {
+function startup() {
     ctx = document.getElementById('myCanvas').getContext("2d");
 
     $('#myCanvas').mousedown(function (e) {
